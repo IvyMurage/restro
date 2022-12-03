023f0711c11d793e0afaf8fe077327166102c0f0
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-12-03 10:33:41 +0300

    Create restaurant container and card files

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <HeroSection />
-      <SignUp />
+      {/* <HeroSection /> */}
+      {/* <SignUp /> */}
       <LoginForm />
       <Footer />
     </div>
