e99058a6df352820c207be016813ed9a27189b2a
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-12-03 08:56:40 +0300

    Style login form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./LoginForm.css"
+import "./LoginForm.css";
 
 export default function LoginForm() {
   return (
@@ -10,15 +10,26 @@ export default function LoginForm() {
       <form className="login-form">
         <label htmlFor="username"> Username</label>
         <br />
-        <input type="text" name="username" />
+        <input
+          type="text"
+          name="username"
+          className="login-input"
+          autoComplete="off"
+        />
         <br />
         <label htmlFor="password"> Password</label>
         <br />
-        <input type="password" name="password" />
-        <br/>
+        <input
+          type="password"
+          name="password"
+          className="login-input"
+          autoComplete="current-password"
+        />
+        <br />
         <button type="submit" className="login-btn">
           Login
         </button>
+        <span id="signup"> Don't have an account? Login</span>
       </form>
     </div>
   );
