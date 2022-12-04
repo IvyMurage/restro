d8f6f85744dd5b3ca5871cd81bfcb364602a83ce
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-12-04 14:04:52 +0300

    Update styles for the sign up form

--- src/Components/RestaurantContext.js
+++ src/Components/RestaurantContext.js
@@ -74,24 +74,7 @@ function RestaurantProvider({ children }) {
       [name]: value,
     });
   }
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   setIsLoading(true);
-  //   fetch("/login", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({ username, password }),
-  //   }).then((r) => {
-  //     setIsLoading(false);
-  //     if (r.ok) {
-  //       r.json().then((user) => onLogin(user));
-  //     } else {
-  //       r.json().then((err) => setErrors(err.errors));
-  //     }
-  //   });
-  // }
+
   async function handleSubmitLoginDetails(event) {
     event.preventDefault();
     setIsLoading(true);
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Signup.css";
 
 function SignUp() {
+  const [signupData, setSignupData] = useState({
+    username: "ChichiiBobo",
+    password: "123",
+    image_url: "",
+    password_confirmation: "123",
+  });
+
+  const [signupError, setSignupError] = useState([]);
+
+  function handleSignupChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setSignupData({ ...signupData, [name]: value });
+  }
   return (
     <div className="signup-div">
       <form className="signup-form">
@@ -11,6 +26,8 @@ function SignUp() {
           name="username"
           autoComplete="off"
           className="signup-input"
+          value={signupData.username}
+          onChange={handleSignupChange}
         />
         <br />
         <label htmlFor="password"> Password </label> <br />
@@ -19,6 +36,8 @@ function SignUp() {
           name="password"
           autoComplete="current-password"
           className="signup-input"
+          value={signupData.password}
+          onChange={handleSignupChange}
         />
         <br />
         <label type="password"> Password Confirmation </label> <br />
@@ -27,15 +46,26 @@ function SignUp() {
           name="password-confirmation"
           autoComplete="current-password"
           className="signup-input"
+          value={signupData.password_confirmation}
+          onChange={handleSignupChange}
         />
         <br />
         <label type="imageUrl"> Profile Image </label> <br />
-        <input type="text" id="imageUrl" className="signup-input" />
+        <input
+          type="text"
+          id="imageUrl"
+          className="signup-input"
+          name="image_url"
+          value={signupData.image_url}
+          onChange={handleSignupChange}
+        />
         <br />
         <button type="submit" id="signup-btn">
           Sign Up
         </button>
-        <span id="login-section"> Have an account already ? Login </span>
+        <Link to={"/login"}>
+          <span id="login-section"> Have an account already ? Login </span>
+        </Link>
       </form>
     </div>
   );
