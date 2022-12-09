ec8254dd06d69fec3214498d5fe93734a9423a74
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-12-09 10:53:51 +0300

    Add an update functionality

--- src/Components/ReviewFolder/ReviewCard.js
+++ src/Components/ReviewFolder/ReviewCard.js
@@ -2,15 +2,36 @@ import React, { useContext } from "react";
 import "./Review.css";
 import { Icon } from "@iconify/react";
 import { RestaurantContext } from "../RestaurantContext";
+import { useState } from "react";
 
 function ReviewCard({ reviewTitle, reviewComment, reviewUser, reviewId }) {
-  const { handleDeleteReview, user } = useContext(RestaurantContext);
+  const { handleDeleteReview, user, restaurantId } =
+    useContext(RestaurantContext);
+  const [update, setUpdate] = useState({
+    comment: "",
+  });
+
+  function handleUpdate(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setUpdate({ [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch(
+      `https://restro-backend-production.up.railway.app/restaurants/${restaurantId}/reviews/${reviewId}`
+    )
+      .then((response) => response.json())
+      .then((data) => setUpdate(data));
+  }
+
   return (
     <div className="review-card">
       <div className="review-info">
         <div className="reviewer-info">
           {/* <h4>{reviewUser.username}</h4>
-          <img src={reviewUser.image_url} alt={reviewUser.username} /> */}
+                <img src={reviewUser.image_url} alt={reviewUser.username} /> */}
         </div>
         {user ? (
           <Icon
@@ -19,10 +40,17 @@ function ReviewCard({ reviewTitle, reviewComment, reviewUser, reviewId }) {
             onClick={() => handleDeleteReview(reviewId)}
           />
         ) : null}
+        <form id="review-form" onSubmit={handleSubmit}>
+          <label htmlFor="title"> Title </label> <br />
+          <input
+            type="text"
+            name="comment"
+            value={update.comment}
+            onChange={handleUpdate}
+          />
+        </form>
       </div>
-
-      <h3> {reviewTitle}</h3>
-      <p>{reviewComment}</p>
+      <h3> {reviewTitle} </h3> <p> {reviewComment} </p>
     </div>
   );
 }
